7176b82acad2ba3a05eefa073249aef25eb7df50
jaydeep <jaydesh1@gmail.com>
2016-02-04 17:31:16 -0500

    "calculation of design bounds"

--- directradiation.m
+++ directradiation.m
@@ -2,10 +2,16 @@ clc;
 clear all;
 close all;
 i0=1000;
+mdot=10;
+cp=950;
+eta=0.9;
+dT=50;
+energy=mdot*cp*dT/(eta);
 alpha = 1;
-f = @(theta,l) sin(theta).*(1-exp(-alpha.*l./sin(theta)));
-q = integral(@(theta) f(theta, 5),0, pi/2);
-dir = @(t) i0.*q./(alpha*pi.*t);
-dir(0.5)
+f = @(theta,l) sin(theta).*(1-exp(-alpha*l./sin(theta)));
+q = @(l) integral(@(theta) f(theta, l),0, pi/2);
+dir = @(w,l) 2*w*i0*q(l)/(alpha*pi);
+findval = @(w) (energy - dir(w,1.4));
+wsolve = fsolve(findval, 0.8)
 % findval= @(l) (100*1000*50)-integral(@(theta) f(theta,l,0.005),0,pi/2);
 % tsolve= fsolve(findval,0.5)
