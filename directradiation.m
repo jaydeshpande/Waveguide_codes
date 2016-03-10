0cfbe7e27a1aee8fe03091bb30aa3dd75a95346c
jaydeep <jaydesh1@gmail.com>
2016-03-10 15:58:31 -0500

    "direct radiation "

--- directradiation.m
+++ directradiation.m
@@ -4,9 +4,10 @@ mdot=1;
 cp=950;
 eta=0.9;
 dT=50;
+ng=100;
 for k=1:1:4
-energy=mdot*cp*dT/(10*eta);
-alpha = 0.05;
+energy=mdot*cp*dT/(ng*eta);
+alpha = 1;
 f = @(theta,l) sin(theta).*(1-exp(-alpha*l./sin(theta)));
 q = @(l) integral(@(theta) f(theta, l),0, pi/2);
 dir = @(w,l) 2*w*i0*q(l)/(alpha*pi);
@@ -21,7 +22,7 @@ for l=0.5:0.5:5
 end
 plot(op(:,1),op(:,2));
 sheet=k;
-dlmwrite('10guidesalpha0.05.csv',op,'-append');
+% dlmwrite('100guidesalpha1.csv',op,'-append');
 hold on;
 dT=dT+50;
 end
