843e7e56dd99324b3e927ad61bfa803f83021951
jaydeep <jaydesh1@gmail.com>
2016-03-10 19:23:22 -0500

    "updated input variables "

--- find_integral.m
+++ find_integral.m
@@ -1,5 +1,5 @@
 function [I]=find_integral(alpha,l)
-f = @(theta,l) (1-exp(-alpha*l/sin(theta)))/(alpha*l/sin(theta));
+f = @(theta,l) (1-exp(-alpha*l./sin(theta)))./(alpha*l./sin(theta));
 q = @(l) integral(@(theta) f(theta, l),0, pi/2);
 I= q(l);
 end
