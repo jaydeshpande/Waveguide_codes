2092b7f31c9ca98c2b2375a993e8420d6aa43b72
jaydeep <jaydesh1@gmail.com>
2016-03-10 19:23:52 -0500

    "updated input variables "

--- find_temperature_profile_conduction.m
+++ find_temperature_profile_conduction.m
@@ -1,13 +1,33 @@
-function [T]=find_temperature_profile_conduction(Re,Pr,D,L)
+function [T]=find_temperature_profile_conduction(input)
+% give me all input values please
+i0 = input(1);
+h = input(2);
+l = input(3);
+m = input(4);
+alpha = input(5);
+Re = input(6);
+Pr = input(7);
+D = input(8);
+Tr = input(9);
+Tinf = input(10);
+n = input(11);
+t = D;
+k = (h*l*l)/(t*m*m);
+% thank you! 
+Trbar = find_Tbar(Tr,input);
 if (Re<2000)
-Nu=find_nusselt_laminar(Re,Pr,D,L);
+Nu=find_nusselt_laminar(Re,Pr,D,l);
 else
 Nu=find_nusselt_turbulent(Re,Pr);
 end
-tsnum=(1/(m*m))-((Nu/m)*Tr*coth(m/2));
+tsnum=(1/(m*m))-((Nu/m)*Trbar*coth(m/2));
 tsden=1-(Nu*coth(m/2)/m);
-Ts=tsnum/tsden;
-firstterm=(i0*l^2)*(1-((2/pi)*find_integral(l)))/(k*t);
-secondterm=((Nu/m)*(Ts-Tr)*(cosh(m(xbar-0.5))/sinh(m/2)))+(1/(m*m));
-T=Tinf + firstterm*secondterm;
+Tsbar=tsnum/tsden;
+firstterm=(i0*l*l)*(1-((2/pi)*find_integral(alpha,l)))/(k*t);
+x=0;
+for i=1:1:n
+    xbar = x/l;
+    secondterm=((Nu/m)*(Tsbar-Trbar)*(cosh(m*(xbar-0.5))/sinh(m/2)))+(1/(m*m));
+    T(i)=Tinf + firstterm*secondterm;
+    x = x + (l/(n-1));
 end
