4100817f0302857c0ecdeecac8ac882448102fd7
jaydeep <jaydesh1@gmail.com>
2016-03-22 16:54:17 -0400

    ""

--- initialize_input.m
+++ initialize_input.m
@@ -1,16 +1,16 @@
-function [input]=initialize_input(l,t)
+function [input]=initialize_input(l)
 i0=1000;
 h=1.85;
 k=1.11;
-l=5;
-alpha=0.20857;
+%l=1;
+alpha=0.2;
 n=201;
 t=0.01;
 D=t;
 m=sqrt(h*l*l/(k*t));
-Re=100;
+Re=1000;
 Pr=7.56; % for water at 17 degree C
-Tr=373;
+Tr=298;
 Tinf=298;
 input=[i0,h,l,m,alpha,Re,Pr,D,Tr,Tinf,n];
 end
