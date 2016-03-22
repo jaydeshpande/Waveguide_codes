9cbb13e08968d84a61e7c5fd524f94d25673d3ab
jaydeep <jaydesh1@gmail.com>
2016-03-22 16:53:59 -0400

    ""

--- directradiation.m
+++ directradiation.m
@@ -1,30 +1,19 @@
-clc;
-i0=1000;
-mdot=1;
-cp=950;
-eta=0.9;
-dT=50;
-ng=100;
-for k=1:1:4
-energy=mdot*cp*dT/(ng*eta);
-alpha = 1;
-f = @(theta,l) sin(theta).*(1-exp(-alpha*l./sin(theta)));
-q = @(l) integral(@(theta) f(theta, l),0, pi/2);
-dir = @(w,l) 2*w*i0*q(l)/(alpha*pi);
-op=zeros();
-i=1;
-for l=0.5:0.5:5
-    findval = @(w) (energy - dir(w,l));
-    wsolve = fsolve(findval, 1);
-    op(i,1) = l;
-    op(i,2) = wsolve;
-    i=i+1;
+function [cc]=directradiation(input)
+% give me all input values please
+i0 = input(1);
+h = input(2);
+l = input(3);
+m = input(4);
+alpha = input(5);
+Re = input(6);
+Pr = input(7);
+D = input(8);
+Tr = input(9);
+Tinf = input(10);
+n = input(11);
+t = D;
+k = (h*l*l)/(t*m*m);
+% thank you! 
+Il=(i0*l/(pi*t))*find_integral(alpha,l);
+cc=Il/i0;
 end
-plot(op(:,1),op(:,2));
-sheet=k;
-% dlmwrite('100guidesalpha1.csv',op,'-append');
-hold on;
-dT=dT+50;
-end
-% findval= @(l) (100*1000*50)-integral(@(theta) f(theta,l,0.005),0,pi/2);
-% tsolve= fsolve(findval,0.5)
